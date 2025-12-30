c609e459e73f2d590849ee3748d6075b8bc95db0
Bhavesh <bhaveshnankani55@gmail.com>
2025-12-30 19:35:28 +0000

    Enabled web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Bhavesh Nankani | Portfolio",
@@ -23,6 +24,7 @@ export default function RootLayout({
       </head>
       <body className="antialiased bg-vscode-bg font-sans">
         {children}
+        <Analytics />
       </body>
     </html>
   );
